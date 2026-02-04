54770084c79cb7e18ab783cf284df1b3889de7d0
Seven Du <5564821+medz@users.noreply.github.com>
2026-02-04 13:14:27 +0800

    refactor: share benchmark setup

--- bench/roux.dart
+++ bench/roux.dart
@@ -1,6 +1,40 @@
 import 'package:benchmark_harness/benchmark_harness.dart';
 import 'package:roux/roux.dart';
 
+RouterContext<String> _buildBenchRouter() {
+  final router = createRouter<String>();
+  addRoute(router, 'GET', '/users', 'users');
+  addRoute(router, 'GET', '/users/:id', 'user');
+  addRoute(router, 'GET', '/users/:id/posts/:postId', 'post');
+  addRoute(router, 'GET', '/files/:filename/*', 'file-format');
+  addRoute(router, 'GET', '/files/:filename', 'file');
+  addRoute(router, 'GET', '/assets/**', 'assets');
+  addRoute(router, 'GET', '/docs/**:path', 'docs');
+  addRoute(router, 'GET', '/test/foo', 'foo');
+  addRoute(router, 'GET', '/test/foo/*', 'foo-param');
+  addRoute(router, 'GET', '/test/foo/**', 'foo-wild');
+  addRoute(router, 'GET', '/wildcard/**', 'wild');
+  addRoute(router, 'GET', '/**', 'root-wild');
+  return router;
+}
+
+List<String> _buildBenchPaths() {
+  return <String>[
+    '/users',
+    '/users/123',
+    '/users/123/posts/456',
+    '/files/report',
+    '/files/report/pdf',
+    '/assets/css/app.css',
+    '/docs/guide/getting-started',
+    '/test/foo',
+    '/test/foo/123',
+    '/test/foo/123/456',
+    '/wildcard/foo/bar',
+    '/anything/goes/here',
+  ];
+}
+
 class FindRouteBench extends BenchmarkBase {
   FindRouteBench() : super('findRoute');
 
@@ -9,34 +43,8 @@ class FindRouteBench extends BenchmarkBase {
 
   @override
   void setup() {
-    router = createRouter<String>();
-    addRoute(router, 'GET', '/users', 'users');
-    addRoute(router, 'GET', '/users/:id', 'user');
-    addRoute(router, 'GET', '/users/:id/posts/:postId', 'post');
-    addRoute(router, 'GET', '/files/:filename/*', 'file-format');
-    addRoute(router, 'GET', '/files/:filename', 'file');
-    addRoute(router, 'GET', '/assets/**', 'assets');
-    addRoute(router, 'GET', '/docs/**:path', 'docs');
-    addRoute(router, 'GET', '/test/foo', 'foo');
-    addRoute(router, 'GET', '/test/foo/*', 'foo-param');
-    addRoute(router, 'GET', '/test/foo/**', 'foo-wild');
-    addRoute(router, 'GET', '/wildcard/**', 'wild');
-    addRoute(router, 'GET', '/**', 'root-wild');
-
-    paths = <String>[
-      '/users',
-      '/users/123',
-      '/users/123/posts/456',
-      '/files/report',
-      '/files/report/pdf',
-      '/assets/css/app.css',
-      '/docs/guide/getting-started',
-      '/test/foo',
-      '/test/foo/123',
-      '/test/foo/123/456',
-      '/wildcard/foo/bar',
-      '/anything/goes/here',
-    ];
+    router = _buildBenchRouter();
+    paths = _buildBenchPaths();
   }
 
   @override
@@ -55,34 +63,8 @@ class FindAllRoutesBench extends BenchmarkBase {
 
   @override
   void setup() {
-    router = createRouter<String>();
-    addRoute(router, 'GET', '/users', 'users');
-    addRoute(router, 'GET', '/users/:id', 'user');
-    addRoute(router, 'GET', '/users/:id/posts/:postId', 'post');
-    addRoute(router, 'GET', '/files/:filename/*', 'file-format');
-    addRoute(router, 'GET', '/files/:filename', 'file');
-    addRoute(router, 'GET', '/assets/**', 'assets');
-    addRoute(router, 'GET', '/docs/**:path', 'docs');
-    addRoute(router, 'GET', '/test/foo', 'foo');
-    addRoute(router, 'GET', '/test/foo/*', 'foo-param');
-    addRoute(router, 'GET', '/test/foo/**', 'foo-wild');
-    addRoute(router, 'GET', '/wildcard/**', 'wild');
-    addRoute(router, 'GET', '/**', 'root-wild');
-
-    paths = <String>[
-      '/users',
-      '/users/123',
-      '/users/123/posts/456',
-      '/files/report',
-      '/files/report/pdf',
-      '/assets/css/app.css',
-      '/docs/guide/getting-started',
-      '/test/foo',
-      '/test/foo/123',
-      '/test/foo/123/456',
-      '/wildcard/foo/bar',
-      '/anything/goes/here',
-    ];
+    router = _buildBenchRouter();
+    paths = _buildBenchPaths();
   }
 
   @override
