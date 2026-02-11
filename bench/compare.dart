4f0f0e00a19dfb1cbc4bc7e1b451aeb195d86856
Seven Du <5564821+medz@users.noreply.github.com>
2026-02-11 17:53:10 +0800

    chore(bench): add relic to comparative benchmark suite

--- bench/compare.dart
+++ bench/compare.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:benchmark_harness/benchmark_harness.dart';
+import 'package:relic/relic.dart' as relic;
 import 'package:roux/roux.dart' as roux;
 import 'package:routingkit/routingkit.dart' as routingkit;
 
@@ -173,11 +174,80 @@ class _RoutingkitLookupDynamicBenchmark extends _RouterBenchmark {
   }
 }
 
+class _RelicAddStaticBenchmark extends _RouterBenchmark {
+  _RelicAddStaticBenchmark() : super('Add;Static;x${_indexes.length}', 'Relic');
+
+  @override
+  void run() {
+    final router = relic.Router<int>();
+    for (final i in _indexes) {
+      router.get('/path$i', i);
+    }
+  }
+}
+
+class _RelicLookupStaticBenchmark extends _RouterBenchmark {
+  _RelicLookupStaticBenchmark()
+    : super('Lookup;Static;x${_indexes.length}', 'Relic');
+
+  late final relic.Router<int> _router;
+
+  @override
+  void setup() {
+    _router = relic.Router<int>();
+    for (final i in _indexes) {
+      _router.get('/path$i', i);
+    }
+  }
+
+  @override
+  void run() {
+    for (final route in _staticRoutesToLookup) {
+      _sink ^= _router.lookup(relic.Method.get, route).asMatch.value;
+    }
+  }
+}
+
+class _RelicAddDynamicBenchmark extends _RouterBenchmark {
+  _RelicAddDynamicBenchmark()
+    : super('Add;Dynamic;x${_indexes.length}', 'Relic');
+
+  @override
+  void run() {
+    final router = relic.Router<int>();
+    for (final i in _indexes) {
+      router.get('/users/:id/items/:itemId/profile$i', i);
+    }
+  }
+}
+
+class _RelicLookupDynamicBenchmark extends _RouterBenchmark {
+  _RelicLookupDynamicBenchmark()
+    : super('Lookup;Dynamic;x${_indexes.length}', 'Relic');
+
+  late final relic.Router<int> _router;
+
+  @override
+  void setup() {
+    _router = relic.Router<int>();
+    for (final i in _indexes) {
+      _router.get('/users/:id/items/:itemId/profile$i', i);
+    }
+  }
+
+  @override
+  void run() {
+    for (final route in _dynamicRoutesToLookup) {
+      _sink ^= _router.lookup(relic.Method.get, route).asMatch.value;
+    }
+  }
+}
+
 void main(List<String> args) {
   final routeCount = _parseArg(args, 0, _defaultRouteCount);
   _setupBenchmarkData(routeCount);
 
-  print('roux vs routingkit benchmark (benchmark_harness)');
+  print('roux vs routingkit vs relic benchmark (benchmark_harness)');
   print('routeCount=$routeCount seed=123');
   print('lower is better (us)');
 
@@ -185,12 +255,16 @@ void main(List<String> args) {
   for (final bench in <_RouterBenchmark>[
     _RouxAddStaticBenchmark(),
     _RoutingkitAddStaticBenchmark(),
+    _RelicAddStaticBenchmark(),
     _RouxLookupStaticBenchmark(),
     _RoutingkitLookupStaticBenchmark(),
+    _RelicLookupStaticBenchmark(),
     _RouxAddDynamicBenchmark(),
     _RoutingkitAddDynamicBenchmark(),
+    _RelicAddDynamicBenchmark(),
     _RouxLookupDynamicBenchmark(),
     _RoutingkitLookupDynamicBenchmark(),
+    _RelicLookupDynamicBenchmark(),
   ]) {
     final score = bench.measure();
     results[bench.name] = score;
@@ -198,31 +272,58 @@ void main(List<String> args) {
   }
 
   print('\nrelative (routingkit / roux, >1 means roux is faster)');
-  _printRatio(
+  _printRoutingkitRatio(
     'add static',
     results['Add;Static;x$routeCount;Routingkit']!,
     results['Add;Static;x$routeCount;Roux']!,
   );
-  _printRatio(
+  _printRoutingkitRatio(
     'lookup static',
     results['Lookup;Static;x$routeCount;Routingkit']!,
     results['Lookup;Static;x$routeCount;Roux']!,
   );
-  _printRatio(
+  _printRoutingkitRatio(
     'add dynamic',
     results['Add;Dynamic;x$routeCount;Routingkit']!,
     results['Add;Dynamic;x$routeCount;Roux']!,
   );
-  _printRatio(
+  _printRoutingkitRatio(
     'lookup dynamic',
     results['Lookup;Dynamic;x$routeCount;Routingkit']!,
+    results['Lookup;Dynamic;x$routeCount;Roux']!,
+  );
+
+  print('\nrelative (relic / roux, >1 means roux is faster)');
+  _printRelicRatio(
+    'add static',
+    results['Add;Static;x$routeCount;Relic']!,
+    results['Add;Static;x$routeCount;Roux']!,
+  );
+  _printRelicRatio(
+    'lookup static',
+    results['Lookup;Static;x$routeCount;Relic']!,
+    results['Lookup;Static;x$routeCount;Roux']!,
+  );
+  _printRelicRatio(
+    'add dynamic',
+    results['Add;Dynamic;x$routeCount;Relic']!,
+    results['Add;Dynamic;x$routeCount;Roux']!,
+  );
+  _printRelicRatio(
+    'lookup dynamic',
+    results['Lookup;Dynamic;x$routeCount;Relic']!,
     results['Lookup;Dynamic;x$routeCount;Roux']!,
   );
   print('sink=$_sink');
 }
 
-void _printRatio(String label, double routingkit, double roux) {
+void _printRoutingkitRatio(String label, double routingkit, double roux) {
   final ratio = routingkit / roux;
+  print('${label.padRight(14)} ${ratio.toStringAsFixed(2)}x');
+}
+
+void _printRelicRatio(String label, double relicValue, double roux) {
+  final ratio = relicValue / roux;
   print('${label.padRight(14)} ${ratio.toStringAsFixed(2)}x');
 }
 
